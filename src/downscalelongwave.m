ba7c7d20b22658216960f416705b8c1fb6905b7e
mchl-mccrthy <62691367+mchl-mccrthy@users.noreply.github.com>
2026-01-13 13:28:34 +0100

    Update downscalelongwave.m
    Fixed unit error in vapour pressure equations

--- src/downscalelongwave.m
+++ src/downscalelongwave.m
@@ -23,17 +23,17 @@ TdInt = fnval(fnSpline,dsXys);
 TdInt = TdInt(:);
 TdInt = reshape(TdInt,size(dsX));
 
-% Calculate interpolated and downscaled vapour pressure (Pa)
-eInt = 611.*exp(17.27.*TdInt./(237.3+TdInt));
-dsE = 611.*exp(17.27.*dsTd./(237.3+dsTd));
-
-% Convert to hPa
-eInt = eInt/100;
-dsE = dsE/100;
+% Calculate interpolated and downscaled vapour pressure (hPa)
+eInt = 6.112.*exp(17.27.*(TdInt-273.15)./(237.3+TdInt-273.15));
+dsE = 6.112.*exp(17.27.*(dsTd-273.15)./(237.3+dsTd-273.15));
 
 % Calculate interpolated and downscaled emmisivity
 epsInt = 1.08.*(1-exp(-eInt.^(TInt./2016)));
 dsEps = 1.08.*(1-exp(-dsE.^(dsT./2016)));
+
+% Limit emissivity to between zero and one
+epsInt = min(max(epsInt,0),1);
+dsEps  = min(max(dsEps,0),1);
 
 % Calculate downscaled longwave
 sigma = 5.67e-8;
